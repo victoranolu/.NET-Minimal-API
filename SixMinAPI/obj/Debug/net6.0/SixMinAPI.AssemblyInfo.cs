76791c8a39be888d395bfd7dd1576924b8612b35
Victor Anolu <afactor05@gmail.com>
2024-01-23 16:56:57 +0100

    added paackages

--- SixMinAPI/obj/Debug/net6.0/SixMinAPI.AssemblyInfo.cs
+++ SixMinAPI/obj/Debug/net6.0/SixMinAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9a77c5da-d671-45a9-8605-acb2d1cfd849")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SixMinAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
