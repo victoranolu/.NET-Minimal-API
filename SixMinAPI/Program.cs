2ac903f4bbab86589789b8f45fa19dc91b55bb77
victoranolu <afactor05@gamil.com>
2024-01-31 13:37:17 +0100

    setting up the sql connection into the program

--- SixMinAPI/Program.cs
+++ SixMinAPI/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFramework;
+using SixMinAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConBuilder = new SqlConnectionStringBuilder();
+
+sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConBuilder.User.ID = builder.Configuration["UserId"];
+sqlConBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
 
 var app = builder.Build();
 
